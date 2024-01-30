9a91b285c27467f1befa3fd2e4932fa961e2bdc3
ajay <ramgaa307@gmail.com>
2024-01-30 15:55:34 +0530

    postgresql init

---
+++ backend/config/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  database: {
+    databaseType: process.env.DATABASE_TYPE ?? 'postgres',
+    databaseHost: process.env.DATABASE_HOST ?? 'localhost',
+    databasePort: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    databasePassword: process.env.DATABASE_PASSWORD ?? '',
+    databaseUsername: process.env.DATABASE_USERNAME ?? 'postgres',
+    databaseName: process.env.DATABASE_NAME ?? 'postgres',
+  },
+});
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      envFilePath: '../.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get('database').databaseType,
+        host: configService.get('database').dbHostname,
+        port: configService.get('database').databasePort,
+        database: configService.get('database').databaseName,
+        password: configService.get('database').databasePassword,
+        username: configService.get('database').databaseUsername,
+        entities: [],
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
