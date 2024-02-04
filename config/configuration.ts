a6eaba9889cdfda02ac800fff7dc1eb5c75e5590
ajay <ramgaa307@gmail.com>
2024-02-04 19:04:37 +0530

    minor fixes in env

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,7 +6,7 @@ const configuration = () => ({
   },
   database: {
     databaseType: process.env.DATABASE_TYPE ?? 'postgres',
-    databaseHost: process.env.DATABASE_HOST ?? 'localhost',
+    dbHostname: process.env.DATABASE_HOST ?? 'localhost',
     databasePort: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
     databasePassword: process.env.DATABASE_PASSWORD ?? '',
     databaseUsername: process.env.DATABASE_USERNAME ?? 'postgres',
